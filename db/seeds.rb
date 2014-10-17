98340a4961cfcb5322990854dd4c41d4b447aaf5
Mark Scala <markscala@gmail.com>
2014-10-17 17:19:51 -0500

    display home page and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "Family Guy", description: "Cartoon for adults.", small_cover_url: "/tmp/family_guy.jpg")
+Video.create(title: "Monk", description: "Not really sure what this is.", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
+Video.create(title: "Peaky Blinders", description: "Post WWI street gangs in Manchester.", small_cover_url: "/tmp/south_park.jpg")
