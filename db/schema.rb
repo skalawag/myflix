2cc0d12686b184cd2edb2c3575aeb77a2b4325ef
Mark Scala <markscala@gmail.com>
2014-10-18 08:46:21 -0500

    models and migrations for categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017205813) do
+ActiveRecord::Schema.define(version: 20141018134411) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
+
+  create_table "video_categories", force: true do |t|
+    t.integer "video_id"
+    t.integer "category_id"
+  end
 
   create_table "videos", force: true do |t|
     t.string   "title"
