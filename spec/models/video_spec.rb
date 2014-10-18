073e07c84e6bfa7582e7de4c9011c56dd8f316ea
Mark Scala <markscala@gmail.com>
2014-10-18 16:00:12 -0500

    test validations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -15,4 +15,22 @@ describe Video do
 
     expect(Video.first.categories.first).to eq(cat)
   end
+
+  it "validates title" do
+    v = Video.create(description: "Hello")
+
+    expect(v.valid?).to eq(false)
+  end
+
+  it "validates description" do
+    v = Video.create(title: "Hello")
+
+    expect(v.valid?).to eq(false)
+  end
+
+  it "valid when both title and description are present" do
+    v = Video.create(title: "Hello", description: "World")
+
+    expect(v.valid?).to eq(true)
+  end
 end
