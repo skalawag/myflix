c97b9e92809bb99b8a605d81b7a2c0bd7711cffd
Mark Scala <markscala@gmail.com>
2014-10-18 09:23:49 -0500

    new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: "Family Guy", description: "Cartoon for adults.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/blank_large.jpg")
-Video.create(title: "Monk", description: "Not really sure what this is.", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "Peaky Blinders", description: "Post WWI street gangs in Manchester.", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/blank_large.jpg")
-Video.create(title: "Futurama", description: "Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/blank_large.jpg")
+["TV Shows", "Action & Adventure", "Anime", "Children & Family", "Classics", "Comedies", "Cult Movies", "Documentaries", "Dramas", "Faith & Spirituality", "Foreign", "Gay & Lesbian", "Horror", "Independent", "Music", "Musicals", "Romance", "Sci-Fi & Fantasy", "Thrillers"].each do |category|
+  Category.create(name: category)
+end
+
+fg = Video.create(title: "Family Guy", description: "Cartoon for adults.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/blank_large.jpg")
+monk = Video.create(title: "Monk", description: "Not really sure what this is.", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
+pb = Video.create(title: "Peaky Blinders", description: "Post WWI street gangs in Manchester.", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/blank_large.jpg")
+fut = Video.create(title: "Futurama", description: "Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/blank_large.jpg")
+
+fg.categories << Category.find_by(name: "Comedies")
+fg.categories << Category.find_by(name: "TV Shows")
+monk.categories << Category.find_by(name: "TV Shows")
+pb.categories << Category.find_by(name: "TV Shows")
+pb.categories << Category.find_by(name: "Dramas")
+pb.categories << Category.find_by(name: "Foreign")
+fut.categories << Category.find_by(name: "Comedies")
+fut.categories << Category.find_by(name: "Comedies")
