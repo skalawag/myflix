e2a61838700fa3a1345d47c7038e208d418459ea
Mark Scala <markscala@gmail.com>
2014-10-18 07:49:00 -0500

    added blank_large.jpg to public/tmp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Myflix::Application.routes.draw do
+  root to: 'ui#index'
   get 'ui(/:action)', controller: 'ui'
 
   get '/home', to: 'videos#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: "Family Guy", description: "Cartoon for adults.", small_cover_url: "/tmp/family_guy.jpg")
+Video.create(title: "Family Guy", description: "Cartoon for adults.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/blank_large.jpg")
 Video.create(title: "Monk", description: "Not really sure what this is.", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
-Video.create(title: "Peaky Blinders", description: "Post WWI street gangs in Manchester.", small_cover_url: "/tmp/south_park.jpg")
+Video.create(title: "Peaky Blinders", description: "Post WWI street gangs in Manchester.", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/blank_large.jpg")
+Video.create(title: "Futurama", description: "Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/blank_large.jpg")
