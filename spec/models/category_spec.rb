4b5a9c71cd7f46e9f82aab0f77007270c08e8365
Mark Scala <markscala@gmail.com>
2014-10-18 14:30:35 -0500

    test: Category-Video association

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Category do
+  it "has many videos" do
+    v1 = Video.create(title: "Family Guy", description: "Cartoon for adults.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/blank_large.jpg")
+    v2 = Video.create(title: "Monk", description: "Not really sure what this is.", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
+    cat = Category.new(name: "Test Category")
+    v1.categories << cat
+    v2.categories << cat
+    v1.save
+    v2.save
+
+    expect(Category.first.videos).to eq([v1, v2])
+  end
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,3 +7,14 @@ describe Video do
     expect(Video.first).to eq(v)
   end
 end
+
+describe Video do
+  it "has a category" do
+    v = Video.create(title: "Family Guy", description: "Cartoon for adults.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/blank_large.jpg")
+    cat = Category.create(name: "Test Category")
+    v.categories << cat
+    v.save
+
+    expect(Video.first.categories.first).to eq(cat)
+  end
+end
