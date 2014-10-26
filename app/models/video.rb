23fe4f2be3a195152119e2a0f11c67a95a8a0eab
Mark Scala <markscala@gmail.com>
2014-10-26 15:47:23 -0500

    latest

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,8 @@
 class Video < ActiveRecord::Base
   has_many :video_categories
   has_many :categories, through: :video_categories
+
+  def self.search_by_title(title)
+    where("title LIKE #{title}")
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,3 +4,17 @@ describe Video do
   it { should have_many :video_categories }
   it { should have_many :categories }
 end
+
+describe "Video.search_by_title" do
+  it "should return [] if no match is found" do
+    result = Video.search_by_title("Any")
+    expect(result).to eq([])
+  end
+
+  it "should return a list of 1 video for an exact match" do
+    result = Video.search_by_title("Monk")
+    expect(result).to eq(Video.find(2))
+  end
+
+  it "should return a list of all videos making a partial match"
+end
