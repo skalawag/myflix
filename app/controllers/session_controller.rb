4f18ff5e66279fa68f168642c46859dfe2abb840
Mark Scala <markscala@gmail.com>
2014-10-27 10:47:05 -0500

    authentication, logging in

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,8 +4,20 @@ class SessionController < ApplicationController
   end
 
   def create
+    user = User.find_by email: params[:user][:email]
+    if user && user.authenticate(params[:user][:password])
+      redirect_to videos_path
+    else
+      redirect_to new_login_path
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:user).permit(:email, :password)
   end
 end
