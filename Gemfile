3460b1beebc59f6753271b9a1ba16b42af951215
Mark Scala <markscala@gmail.com>
2014-10-27 07:19:31 -0500

    added bcrypt

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'uglifier'
 gem 'jquery-rails'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
+gem 'bcrypt'
 
 group :development do
   gem 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018134411) do
+ActiveRecord::Schema.define(version: 20141027121036) do
 
   create_table "categories", force: true do |t|
     t.string "name"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
   create_table "video_categories", force: true do |t|
