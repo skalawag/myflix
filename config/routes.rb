19d66ccbaed8e11cf7e4b350715dab81e86c1eeb
Mark Scala <markscala@gmail.com>
2014-10-27 07:41:23 -0500

    comments on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Myflix::Application.routes.draw do
   root to: 'ui#index'
   get 'ui(/:action)', controller: 'ui'
 
+  resources :videos
+
+  # synonym for videos
   get '/home', to: 'videos#index'
+  # this is like show:
   get '/video', to: 'videos#video'
+
+  # this is a category route
   get '/genre/:genre', to: 'videos#genre', as: :genre
+
+  # special route
   get '/search', to: 'videos#search_by_title', as: :search
 
 end
