2ed678a66dcfca3317ce05edc7d0bd0b10df9f36
Mark Scala <markscala@gmail.com>
2014-10-27 07:41:15 -0500

    added FIXME

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,9 @@
 class VideosController < ApplicationController
 
   def index
+    # FIXME: This is probably not a good idea. What if we have 100K
+    # videos in the database? Do we really want to hit the db like
+    # that every time someone opens the index page?
     @videos = Video.all
     @categories = Category.all
   end
