22b79e3355587e5c466e5c325425437d2462a467
Mark Scala <markscala@gmail.com>
2014-10-27 07:21:27 -0500

    validates presence of email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  validates_presence_of :email, :username
   has_secure_password validations: false
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe User do
   it { should have_secure_password }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :username }
 end
