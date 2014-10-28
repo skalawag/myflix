bdaaa1e861fd34f5c50afd1aedeb70348296a331
Mark Scala <markscala@gmail.com>
2014-10-28 16:49:00 -0500

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Myflix::Application.routes.draw do
   root to: 'pages#front'
   get 'ui(/:action)', controller: 'ui'
 
-  resources :videos
-
   # synonym for videos
   get '/home', to: 'videos#index'
-  # this is like show:
-  get '/video', to: 'videos#show'
 
+  resource :videos, only: [:index, :show]
   # this is a category route
   get '/genre/:genre', to: 'videos#genre', as: :genre
 
