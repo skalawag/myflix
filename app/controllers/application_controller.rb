d97746f4da1557005ec8d7f0152c7b1dedb86bdf
Mark Scala <markscala@gmail.com>
2014-11-04 16:07:07 -0600

    refactor current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  helper_method :current_user
+
   def require_user
     redirect_to new_login_path unless current_user
   end
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  # in order for any of the methods here to be called from a template,
-  # we need to make them helper methods
-  helper_method :current_user
 end
