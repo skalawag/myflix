163a010851d5956f0f75cfa7768432df307f1949
Mark Scala <markscala@gmail.com>
2014-11-07 08:06:46 -0600

    use Transactions for queue ordering

--- app/models/queued_video.rb
+++ app/models/queued_video.rb
@@ -5,11 +5,13 @@ class QueuedVideo < ActiveRecord::Base
 
   def self.renumber_queued_items(items, uid)
     position_counter = 1
-    items.each do |item|
-      queued_video = QueuedVideo.where(user_id: uid,
-                                       video_id: item["id"])
-      queued_video.first.update(queue_position: position_counter)
-      position_counter += 1
+    ActiveRecord::Base.transaction do
+      items.each do |item|
+        queued_video = QueuedVideo.where(user_id: uid,
+                                         video_id: item["id"])
+        queued_video.first.update(queue_position: position_counter)
+        position_counter += 1
+    end
     end
   end
 end
