8c8d62ada5fea6e548e6eb83aae890e648c51336
Mark Scala <markscala@gmail.com>
2014-11-14 12:07:49 -0600

    users profile page wired up and tested

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -10,8 +10,8 @@ feature "A user's video collection and reviews display on user's page" do
     visit "/users/#{user[:id]}"
 
     expect(page).to have_content("#{user[:username]}'s video collections")
-    expect(page).to have_content("#{user[:username]}'s video collections (user.videos.count)")
+    expect(page).to have_content("#{user[:username]}'s video collections (#{user.videos.count})")
     expect(page).to have_content("#{user[:username]}'s Reviews")
-    expect(page).to have_content("#{user[:username]}'s Reviews (user.reviews.count)")
+    expect(page).to have_content("#{user[:username]}'s Reviews (#{user.reviews.count})")
   end
 end
