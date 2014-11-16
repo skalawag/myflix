f09d49847a4f20504ae6c43a51765fae276d860d
Mark Scala <markscala@gmail.com>
2014-11-16 15:56:04 -0600

    changed email address to wagflix

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,6 +1,6 @@
 class AppMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
-    mail to: user.email, from: 'myflix@gmail.com', subject: 'Welcome!'
+    mail to: user.email, from: 'wagflix@gmail.com', subject: 'Welcome!'
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,13 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'example.com',
-  #   user_name:            '<username>',
-  #   password:             '<password>',
-  #   authentication:       'plain',
-  #     enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            "ENV['username'],
+    password:             "ENV['mail_pass'],
+    authentication:       'plain',
+      enable_starttls_auto: true  }
 end
