3d017dd9245d8ba70e6af729e24b3328c24deeec
Mark Scala <markscala@gmail.com>
2014-11-18 10:33:51 -0600

    set up mailtrap for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,16 @@ Myflix::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: 'localhost:3001' }
+  config.action_mailer.default_url_options = {host: '192.168.1.2:3001'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '2651683f91e05b147',
+    :password => 'e4f71fce4d3a46',
+    :address => 'mailtrap.io',
+    :domain => 'mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -17,6 +25,7 @@ Myflix::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
+
   config.assets.debug = true
 
   config.eager_load = false
