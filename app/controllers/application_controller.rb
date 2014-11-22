bd25ece163eede36532648c0a1fe8d71cc5e2878
Mark Scala <markscala@gmail.com>
2014-11-22 13:14:41 -0600

    delete session if it exists right before we create a new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,7 @@ class ApplicationController < ActionController::Base
     current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def destroy_session
+    session[:user_id] = nil
+  end
 end
