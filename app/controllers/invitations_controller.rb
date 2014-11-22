03f8a42c1bb7fb76fda996e9cace35e3388d5709
Mark Scala <markscala@gmail.com>
2014-11-22 12:00:03 -0600

    removed inviter_email from mail

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,6 @@
 class InvitationsController < ApplicationController
   def create
-    AppMailer.invite_friend(params[:name], params[:email], params[:message], current_user.username, current_user.email).deliver
+    AppMailer.invite_friend(params[:name], params[:email], params[:message], current_user.username).deliver
     flash[:success] = "Your invitation has been sent."
     redirect_to home_path
   end
