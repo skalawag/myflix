d0f2c01a5aae080d53e57943e438cebd47977103
Mark Scala <markscala@gmail.com>
2014-11-22 11:19:00 -0600

    add inviter's email to invitation email so that we can identify her later

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -9,8 +9,8 @@ class AppMailer < ActionMailer::Base
     mail to: user.email, from: 'wagflix@gmail.com', subject: 'Password reset'
   end
 
-  def invite_friend(name, email, message, inviter)
-    @name, @message, @inviter = name, message, inviter
+  def invite_friend(name, email, message, inviter, inviter_email)
+    @name, @message, @inviter, @inviter_email = name, message, inviter, inviter_email
     mail to: email, from: 'wagflix@gmail.com', subject: "An invitation from #{inviter}!"
   end
 end
