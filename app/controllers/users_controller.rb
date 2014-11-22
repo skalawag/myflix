dc04c16b2b7b69b2700b90f26b4072878543b20a
Mark Scala <markscala@gmail.com>
2014-11-22 13:15:40 -0600

    delete an invitation for new user if invite exists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    destroy_session if current_user
     @user = User.new(user_params)
     if @user.valid? && @user.save
       AppMailer.welcome_email(@user).deliver
+      delete_invite_if_invited(@user)
       redirect_to home_path
     else
-      @user.save
       render :new
     end
   end
@@ -22,5 +23,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password, :email)
+  end
+
+  def delete_invite_if_invited(user)
+    if invite = Invitation.find_by(new_user_email: user.email)
+      invite.destroy
+    end
   end
 end
