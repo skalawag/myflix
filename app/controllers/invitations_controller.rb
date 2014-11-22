79be9f19e3caa26a13694167f72ed1607b6b3929
Mark Scala <markscala@gmail.com>
2014-11-22 11:24:54 -0600

    added extra argument to invite_friend call

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,6 @@
 class InvitationsController < ApplicationController
   def create
-    AppMailer.invite_friend(params[:name], params[:email], params[:message], current_user.username).deliver
+    AppMailer.invite_friend(params[:name], params[:email], params[:message], current_user.username, current_user.email).deliver
     redirect_to home_path
   end
 end
