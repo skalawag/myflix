f7d7b05e11c8f761fad5e5ce0714ed7be2efb494
Mark Scala <markscala@gmail.com>
2014-11-22 11:25:35 -0600

    flash success for sent invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,7 @@
 class InvitationsController < ApplicationController
   def create
     AppMailer.invite_friend(params[:name], params[:email], params[:message], current_user.username, current_user.email).deliver
+    flash[:success] = "Your invitation has been sent."
     redirect_to home_path
   end
 end
