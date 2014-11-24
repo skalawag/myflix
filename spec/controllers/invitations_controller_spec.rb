a5319af130605126ce7c851d7f72af34a36c9756
Mark Scala <markscala@gmail.com>
2014-11-24 12:33:59 -0600

    test refactoring

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -14,16 +14,18 @@ describe InvitationsController do
       expect(assigns(:user)).to be_a(User)
     end
 
-    it "has an invitation if one exists" do
-      invite = Fabricate(:invitation)
-      get :show, id: invite.token
-      expect(assigns(:invitation)).to be_a(Invitation)
-    end
+    context "with invitation" do
+      before { Fabricate(:invitation) }
 
-    it "renders show template" do
-      invite = Fabricate(:invitation)
-      get :show, id: invite.token
-      expect(response).to render_template :show
+      it "has an invitation if one exists" do
+        get :show, id: Invitation.first.token
+        expect(assigns(:invitation)).to be_a(Invitation)
+      end
+
+      it "renders show template" do
+        get :show, id: Invitation.first.token
+        expect(response).to render_template :show
+      end
     end
 
     it "redirects to new_user_path if there is no invitation" do
@@ -36,39 +38,36 @@ describe InvitationsController do
     after { ActionMailer::Base.deliveries.clear }
 
     describe "POST create" do
+      before { authenticated_user }
+      let(:user) { User.first }
+
       it "should be valid" do
-        user = authenticated_user
         post :create, name: "Mark Scala", email: "markscala@gmail.com", message: "Hey, join up."
         invite = Invitation.first
         expect(invite).to be_a(Invitation)
       end
 
       it "sets flash success if successful" do
-        user = authenticated_user
         post :create, name: "Mark Scala", email: "markscala@gmail.com", message: "Hey, join up."
         expect(flash[:success]).to eq("Your invitation has been sent.")
       end
 
       it "redirects to home path after successful invitation made" do
-        user = authenticated_user
         post :create, name: "Mark Scala", email: "markscala@gmail.com", message: "Hey, join up."
         expect(response).to redirect_to home_path
       end
 
       it "sets flash error if unsuccessful because email fails" do
-        user = authenticated_user
         post :create, name: "Mark Scala", email: "", message: "Hey, join up."
         expect(flash[:error]).to eq("Something was wrong with your invitation.")
       end
 
       it "sets flash error if unsuccessful because name is missing" do
-        user = authenticated_user
         post :create, name: "", email: "markscala@gmail.com", message: "Hey, join up."
         expect(flash[:error]).to eq("Something was wrong with your invitation.")
       end
 
       it "renders new if unsuccessful" do
-        user = authenticated_user
         post :create, name: "", email: "markscala@gmail.com", message: "Hey, join up."
         expect(response).to render_template :new
       end
--- spec/controllers/reset_password_controller_spec.rb
+++ spec/controllers/reset_password_controller_spec.rb
@@ -7,36 +7,38 @@ describe ResetPasswordController do
       expect(response).to render_template :new
     end
 
-    it "should set the user's token" do
-      user = Fabricate(:user, email: "xyz@xyz.com")
-      post :create, email: "xyz@xyz.com"
-      expect(User.find(user.id).token).to_not eq(nil)
-    end
+    context "with user" do
+      before { Fabricate(:user, email: "xyz@xyz.com") }
+      let(:user) { User.first }
 
-    it "should have display an error message if user's email cannot be found" do
-      user = Fabricate(:user, email: "xyz@xyz.com")
-      post :create, email: "xzz@xyz.com"
-      expect(flash[:error]).to eq("The email you entered is not a valid user's email.")
-    end
+      it "should set the user's token" do
+        post :create, email: "xyz@xyz.com"
+        expect(User.find(user.id).token).to_not eq(nil)
+      end
 
-    it "should render new when email is invalid" do
-      user = Fabricate(:user, email: "xyz@xyz.com")
-      post :create, email: "xzz@xyz.com"
-      expect(response).to render_template :new
+      it "should have display an error message if user's email cannot be found" do
+        post :create, email: "xzz@xyz.com"
+        expect(flash[:error]).to eq("The email you entered is not a valid user's email.")
+      end
+
+      it "should render new when email is invalid" do
+        post :create, email: "xzz@xyz.com"
+        expect(response).to render_template :new
+      end
     end
 
     context "with email" do
+      before { Fabricate(:user) }
+      let(:user) { User.first }
+      let(:msg) { ActionMailer::Base.deliveries.last }
       after { ActionMailer::Base.deliveries.clear }
 
       it "should send email to user if user is confirmed" do
-        user = Fabricate(:user)
         post :create, email: user.email
-        msg = ActionMailer::Base.deliveries.last
         expect(msg.to).to eq([user.email])
       end
 
       it "redirects to reset_password_confirmation page if user is confirmed" do
-        user = Fabricate(:user)
         post :create, email: user.email
         expect(response).to redirect_to reset_password_confirmation_path
       end
