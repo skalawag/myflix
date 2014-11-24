ac8695080707a585fa5f3289be8536b6d43252bf
Mark Scala <markscala@gmail.com>
2014-11-24 16:05:39 -0600

    changed app domain in email config

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,18 @@
 class AppMailer < ActionMailer::Base
+  default from: "markscala@gmail.com"
+
   def welcome_email(user)
     @user = user
-    mail to: user.email, from: 'wagflix@gmail.com', subject: 'Welcome!'
+    mail to: user.email, subject: 'Welcome!'
   end
 
   def reset_password(user)
     @user = user
-    mail to: user.email, from: 'wagflix@gmail.com', subject: 'Password reset'
+    mail to: user.email, subject: 'Password reset'
   end
 
   def invite_friend(name, email, message, inviter, token)
     @name, @message, @inviter, @token = name, message, inviter, token
-    mail to: email, from: 'wagflix@gmail.com', subject: "An invitation from #{inviter}!"
+    mail to: email, subject: "An invitation from #{inviter}!"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Myflix::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'shielded-meadow-4662.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
