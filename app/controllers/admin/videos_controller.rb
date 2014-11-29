fcc8b37362d62a767137df617f46c7345db9bf7b
Mark Scala <markscala@gmail.com>
2014-11-28 18:49:51 -0600

    admin video controller add and create

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -2,12 +2,18 @@ class Admin::VideosController < ApplicationController
   before_filter :require_admin
 
   def new
+    @categories = Category.all.map { |category| "<option>#{category.name}</option>" }.join()
     @video = Video.new
   end
 
   def create
-    @video = Video.new
-    redirect_to home_path
+    @video = Video.new(title: params[:title], description: params[:description])
+    if @video.save
+      redirect_to home_path
+    else
+      flash[:error] = "Something went wrong."
+      redirect_to new_admin_video_path
+    end
   end
 
   private
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Admin::VideosController do
+
+
   describe "GET new" do
     before { authenticated_user }
     let(:user) { User.first }
@@ -10,6 +12,13 @@ describe Admin::VideosController do
         user.update_attribute(:admin, true)
         get :new
         expect(assigns(:video)).to be_a(Video)
+      end
+
+      it "has set @categories instance variable" do
+        user.update_attribute(:admin, true)
+        target = Category.all.map { |category| "<option>#{category.name}</option>" }.join()
+        get :new
+        expect(assigns(:categories)).to eq(target)
       end
 
       it "redirects ordinary user to home path" do
@@ -24,17 +33,33 @@ describe Admin::VideosController do
     end
 
     describe "POST create" do
+      it "sets flash[:error] if user is not admin" do
+        post :create
+        expect(flash[:error]).to eq("You are not authorized to access that page.")
+      end
+    end
+
+    describe "POST create" do
       before { user.update_attribute(:admin, true) }
 
-      it "should redirect to" do
-        get :create
+      it "should redirect to home_path" do
+        post :create, title: "Home", description: "Something"
         expect(response).to redirect_to home_path
       end
 
       it "has @video instance variable set to a new video" do
-        user.update_attribute(:admin, true)
-        get :create
+        post :create, title: "Home", description: "Something"
         expect(assigns(:video)).to be_a(Video)
+      end
+
+      it "sets flash[:error] if video fails to save" do
+        post :create, title: "Home"
+        expect(flash[:error]).to eq("Something went wrong.")
+      end
+
+      it "redirects to new_admin_video_path when it cannot save a video" do
+        post :create, title: "Home"
+        expect(response).to redirect_to new_admin_video_path
       end
     end
   end
