88ce8fdc1958d31f4f56c1bf3719d0a0c4115eea
Mark Scala <markscala@gmail.com>
2014-11-30 14:13:29 -0600

    wagflixstore added to fog config

--- config/initializers/fog_carrierwave_config.rb
+++ config/initializers/fog_carrierwave_config.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],  # required
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
   }
-  config.fog_directory  = 'name_of_directory'                # required
+  config.fog_directory  = 'wagflixstore'
 end
