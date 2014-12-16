32c7ba4685aefd571eafb58959e0b652723c182b
Mark Scala <markscala@gmail.com>
2014-12-16 09:05:23 -0600

    phantomjs working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,9 +24,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-ActionDispatch::IntegrationTest
-  Capybara.server_port = 3090
-  Capybara.app_host = 'http://0.0.0.0:3090'
+# ActionDispatch::IntegrationTest
+#   Capybara.server_port = 3001
+#   Capybara.app_host = 'http://0.0.0.0:3001'
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
